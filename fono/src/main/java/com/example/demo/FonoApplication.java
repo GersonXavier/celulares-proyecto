b726e834f8b9ffeed30d86efa58ab450f4ece320
Your Name <you@example.com>
2021-05-31 23:50:40 -0500

    gerson

---
+++ fono/src/main/java/com/example/demo/FonoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FonoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FonoApplication.class, args);
+	}
+
+}
